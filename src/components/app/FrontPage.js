d8e07c0b91ad77c2ce75153a018af0cf62902f72
TeemuKoivisto <tkoivisto456@gmail.com>
2016-10-26 00:11:59 +0300

    Add cooler styles

--- src/components/app/FrontPage.js
+++ src/components/app/FrontPage.js
@@ -5,9 +5,35 @@ export default class FrontPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>PohinaApp</h1>
-        <p>hey</p>
+      <div className="container">
+        <div className="page-header" id="banner">
+          <div className="row">
+            <div className="col-lg-8 col-md-7 col-sm-6">
+              <h1>Yeti</h1>
+              <p className="lead">A friendly foundation</p>
+            </div>
+            <div className="col-lg-4 col-md-5 col-sm-6">
+              <div className="sponsor">
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-3 col-md-3 col-sm-4">
+              <div className="list-group table-of-contents">
+                <a className="list-group-item" href="#navbar">Navbar</a>
+                <a className="list-group-item" href="#buttons">Buttons</a>
+                <a className="list-group-item" href="#typography">Typography</a>
+                <a className="list-group-item" href="#tables">Tables</a>
+                <a className="list-group-item" href="#forms">Forms</a>
+                <a className="list-group-item" href="#navs">Navs</a>
+                <a className="list-group-item" href="#indicators">Indicators</a>
+                <a className="list-group-item" href="#progress-bars">Progress bars</a>
+                <a className="list-group-item" href="#containers">Containers</a>
+                <a className="list-group-item" href="#dialogs">Dialogs</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -14,10 +14,32 @@ export class NavBar extends React.Component {
 
   renderNonLoggedNav() {
     return (
-      <div className="ui horizontal pointing menu">
-        <Link className="item" to="/">FrontPage</Link>
-        <Link className="item" to="/login">Login</Link>
-        <Link className="item" to="/registration">Register</Link>
+      <div className="navbar navbar-inverse navbar-fixed-top pohina-pad">
+        <div className="container">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">PöhinäApp</Link>
+            <button className="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+          </div>
+          <div className="navbar-collapse collapse" id="navbar-main">
+            <ul className="nav navbar-nav">
+              <li>
+                <Link to="/page">Page</Link>
+              </li>
+              <li>
+                <Link to="/page2">Another page</Link>
+              </li>
+            </ul>
+
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="https://google.com/" target="_blank">Jos taidot ei riitä</a></li>
+            </ul>
+
+          </div>
+        </div>
       </div>
     );
   }
