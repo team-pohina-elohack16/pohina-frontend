ad4a185fe059f12920738f594a3409e4a77cc75e
TeemuKoivisto <tkoivisto456@gmail.com>
2016-10-26 17:07:45 +0300

    Add webpack production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,60 @@
+if (!process.env.NODE_ENV) {
+  require("dotenv").config();
+}
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  devtool: "cheap-module-eval-source-map",
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-hot-middleware/client",
+    "webpack/hot/only-dev-server",
+    "./src/index",
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          'style',
+          'css',
+          'sass',
+          // 'postcss'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, "src"),
+        loader: 'babel'
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_URL: JSON.stringify(process.env.API_URL),
+      },
+    }),
+  ],
+  resolve: {
+    modulesDirectories: ["node_modules", "src"],
+    extensions: ["", ".js"],
+  },
+}
