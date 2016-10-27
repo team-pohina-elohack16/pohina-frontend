2ab4f34eaeb84c97def1daa5a065190d9713b9f5
samutamm <samutamm@gmail.com>
2016-10-27 18:42:45 +0300

    Mock data to forum

--- src/components/forum/Forum.js
+++ src/components/forum/Forum.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Question from './Question';
 
 export default class Forum extends React.Component {
   render() {
+    var questions = [];
+
+    const question1 = {
+      question: "How to hire someone?",
+      answers: [
+        "Pay some money",
+        "And be a nice boss/co-worker"
+      ]
+    };
+
+    questions.push(question1);
     return (
       <div className="pohina-container-m-top">
-        <p>Forum to be implemented.</p>
+        <h2>Questions:</h2>
+        {questions.map((item) => (
+          <Question question={item} key={item.question} />
+        ))}
       </div>
     );
   }
