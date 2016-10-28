35d8eea22b5e2612a13004e8d650ca6001a553e3
emlai <emil.laine@cs.helsinki.fi>
2016-10-28 11:44:36 +0300

    Collapsible questions

--- src/components/forum/Question.js
+++ src/components/forum/Question.js
@@ -15,14 +15,19 @@ export default class Forum extends React.Component {
     const question = this.props.question;
     const content = this.state.content;
     return (
-      <div className="panel panel-default pohina-forum-panel-outmost">
-        <div className="panel-body">
-        <div id="textbox">
-          <p className="pohina-alignleft">{question}</p>
-          <div className="pohina-alignright">
-            <i className="ion-chevron-down"></i>
+      <div className="q-and-a-wrapper">
+        <div className="panel panel-default pohina-forum-panel-outmost pohina-question">
+          <div className="panel-body collapsed" data-toggle="collapse" data-target="#answer1">
+          <div id="textbox">
+            <p className="pohina-alignleft">{question}</p>
+            <div className="pohina-alignright">
+              <i className="ion-chevron-down"></i>
+            </div>
+          </div>
           </div>
         </div>
+        <div className="pohina-forum-panel-outmost pohina-answer collapse" id="answer1">
+          <p>answer here</p>
         </div>
       </div>
     );
